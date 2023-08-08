dc454be0435dbc651f7df59c1345d8c6a6095e9e
KhotsoSelading <k.selading007@gmail.com>
2023-08-08 17:26:34 +0200

    Show and Distroy - Khotso

--- console.py
+++ console.py
@@ -40,18 +40,41 @@ class HBNBCommand(cmd.Cmd):
         """Usage: show <class> <id> or <class>.show(<id>)
         Display the string representation of a class instance of a given id.
         """
-        argl = arg.split()
+        argList = arg.split()
         
-        if len(argl) == 0:
+        if len(argList) == 0:
             print("** class name missing **")
-        elif argl[0] not in HBNBCommand.__classes:
+        elif argList[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
-        elif len(argl) == 1:
+        elif len(argList) == 1:
             print("** instance id missing **")
         else:
-            class_name = argl[0]
-            instance_id = argl[1]
+            class_name = argList[0]
+            instance_id = argList[1]
             objdict = models.storage
+
+    def do_destroy(self, arg):
+        """Usage: destroy <class> <id> or <class>.destroy(<id>)
+        Delete a class instance of a given id."""
+        argList = arg.split()
+
+        if len(argList) == 0:
+            print("** class name missing **")
+        elif argList[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(argList) == 1:
+            print("** instance id missing **")
+        else:
+            class_name = argList[0]
+            instance_id = argList[1]
+            objdict = models.storage.all()
+            instance_key = "{}.{}".format(class_name, instance_id)
+
+            if instance_key not in objdict.keys():
+                print("** no instance found **")
+            else:
+                del objdict[instance_key]
+                models.storage.save()
 
     def emptyline(self):
         pass
